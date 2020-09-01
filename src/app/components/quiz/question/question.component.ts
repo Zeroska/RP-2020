0e856f62d6a81ac009ceb22818b98620107fe91a
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-09-01 18:43:29 +0700

    nothing change

--- src/app/components/quiz/question/question.component.ts
+++ src/app/components/quiz/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { QuestionListDialogComponent } from '../../dialog/quiz/question-list-dialog/question-list-dialog.component';
 
@@ -9,9 +9,15 @@ import { QuestionListDialogComponent } from '../../dialog/quiz/question-list-dia
 })
 export class QuestionComponent implements OnInit {
 
+  @Output() onHelp:EventEmitter<boolean> = new EventEmitter<boolean>();
+
   constructor(private questionDialog:MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  turnOnHelp(){
+    this.onHelp.emit(true)
   }
 
   openQuestionListDialog(){
